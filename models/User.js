fe6cbd16bad718ce92a21a293ecfa69331371bc4
aabha25 <aabha.jog@gmail.com>
2025-01-23 17:25:14 +0530

    Adding models

---
+++ models/User.js
@@ -0,0 +1,33 @@
+import { Model, DataTypes} from "sequelize";
+
+class User extends Model{}
+
+const initUserModel = sequelize => {
+    User.init(
+        {
+            id:{
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+
+            },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        }
+        }
+   ,{sequelize} );
+
+
+return User.sync().then(()=> console.log('Created User Table'))
+.catch(console.error);
+};
+
+export { User, initUserModel };
---
+++ models/progress.js
@@ -0,0 +1,41 @@
+import { Model, DataTypes, BOOLEAN, ARRAY} from "sequelize";
+import {User} from './User.js';
+class Progress extends Model{}
+
+const initProgressModel = sequelize => {
+    Progress.init(
+        {
+        Questionsid: {
+            type:DataTypes.ARRAY(DataTypes.INTEGER),
+          
+
+        },
+        Selectedans:{
+            type:DataTypes.ARRAY(DataTypes.INTEGER),
+          
+        },
+        Correctans:{
+            type:DataTypes.ARRAY(DataTypes.INTEGER),
+           
+        },
+        Marks:{
+            type:DataTypes.INTEGER,
+        },
+        Counter:{
+            type:DataTypes.INTEGER,
+            
+        },
+        isJunior:{
+            type:BOOLEAN
+        }
+        }
+   ,{sequelize} );
+   Progress.belongsTo(User, {
+    foreignKey: 'userid', //taking name of foreign key
+    });
+
+return Progress.sync().then(()=> console.log('Created Progress Table'))
+.catch(console.error);
+};
+
+export { Progress, initProgressModel };
