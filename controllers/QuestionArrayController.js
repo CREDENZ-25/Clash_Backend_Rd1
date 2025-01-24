986cb2f58abdf2369846d27e046fdf6bc9ff7be4
ARJUN GAWANDE <156084218+ArjunGawande@users.noreply.github.com>
2025-01-24 10:04:55 +0530

    Update QuestionArrayController.js

--- controllers/QuestionArrayController.js
+++ controllers/QuestionArrayController.js
@@ -3,27 +3,26 @@ const { progress, question } = require('../models'); // Assuming you have Sequel
 const addQuestion = async (req, res) => {
   /*
     This code defines a route to add a set of random questions to the user's progress.
-    It retrieves 10 random questions from the Question table using Sequelize, 
+    It retrieves all random questions from the Question table using Sequelize, 
     and then inserts these questions into the Progress table along with the user's ID.
     Once the questions are successfully added, it sends a response indicating that the quiz has started.
     If any error occurs during the process, a 500 status with an error message is returned.
   */
 
   const { userId } = req.body;
+try{
+  const questions = await question.findAll({
+    attributes: ['id'], 
+    order: Sequelize.literal('rand()'),
+  });
 
-  try {
-    // Fetch 10 random questions using Sequelize
-    const questions = await question.findAll({
-      order: Sequelize.literal('rand()'), // Random order
-      limit: 10, // Limit to 10 questions
-    });
-
-    // Create a progress record with the userId and the fetched questions
-    const progress = await progress.create({
-      user_id: userId,
-      questionsArray: JSON.stringify(questions), // Store questions as a string or JSON
-    });
-
+  const questionIds = questions.map((q) => q.id);
+ 
+  const progress = await progress.create({
+    user_id: userId,
+    questionsArray: JSON.stringify(questionIds), 
+  });
+  
     res.status(200).json({
       status: 'Quiz started',
     });
