eb346928bbafcb32c3090eeb21e70ec4122a8930
arjun <arjunsgawande123321@gmail.com>
2025-01-24 10:09:01 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME ,
     host: process.env.DB_HOST ,
     dialect: 'postgres',
-    port: process.env.DB_PO
+    port: process.env.DB_PORT
   },
   test: {
     username: process.env.DB_USER ,
--- models/question.js
+++ models/question.js
@@ -1,1 +1,26 @@
-
+import { DataTypes } from 'sequelize';
+export const question = (sequelize) => {
+  return sequelize.define('questions', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    questionText: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+    optionArray: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false, 
+    },
+    answerIdx: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    isJunior: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+};
