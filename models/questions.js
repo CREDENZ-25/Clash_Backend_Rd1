01e8fe0301f9a5dc6d73b697186e48c110d99bc4
arjun <arjunsgawande123321@gmail.com>
2025-01-29 15:53:25 +0530

    main upadte

--- models/questions.js
+++ models/questions.js
@@ -1,27 +1,42 @@
-import { DataTypes } from 'sequelize';
-export const question = (sequelize) => {
-  return sequelize.define('questions', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    questionText: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-    },
-    optionArray: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false, 
-    },
-    answerIdx: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    isJunior: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  });
+import { STRING, INTEGER, BOOLEAN, Model, DataTypes } from 'sequelize';
+
+class MCQ extends Model {}
+
+const initMCQmodel = (sequelize) => {
+    MCQ.init(
+        {
+            id: {
+                type: INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            question: {
+                type: STRING,  // Renamed from "questions" to "question" for clarity
+                allowNull: false,
+            },
+            options: {
+                type: DataTypes.ARRAY(DataTypes.STRING), // Now an array of strings
+                allowNull: false,
+            },
+            correct: {
+                type: INTEGER, // Stores the correct option index
+                allowNull: false,
+                validate: {
+                    min: 0, // Ensures the index is at least 0
+                    max: 3, // Ensures the index is within valid range
+                },
+            },
+            isJunior: {
+                type: BOOLEAN,
+                defaultValue: false, // Default to false
+            },
+        },
+        { sequelize }
+    );
+
+    return MCQ.sync()
+        .then(() => console.log("MCQ table created!"))
+        .catch((err) => console.log("Table not created:", err));
 };
 
+export { MCQ, initMCQmodel };
