165f9427d55dbea15171de8dac2114e57e6f4148
ARJUN GAWANDE <156084218+ArjunGawande@users.noreply.github.com>
2025-02-14 10:05:42 +0530

    Update StartController.js

--- controllers/StartController.js
+++ controllers/StartController.js
@@ -1,33 +1,4 @@
-// // Route to start the test
-// const startTest= async (req, res) => {
-   
-// /*
-// This code starts a test for the user by creating a progress record with the user's ID and the current timestamp. 
-// It then sets a timer to automatically submit the test after 30 minutes. Once the time is up, the test is submitted, 
-// and a response is sent to the client indicating the test is complete. If an error occurs, a 500 status with an error 
-// message is returned.
-//   */
-//   const userId = req.user.id; 
 
-//     try {
-//       const progress = await Progress.create({
-//         userId,
-//         startTime: new Date(),  
-//       });
-  
-//       setTimeout(async () => {
-//         await submitTest(progress.id);  
-//         res.json({
-//           message: 'test competle',
-//         });
-//       }, 1800000); 
-  
-//     } catch (error) {
-//       console.error('Error starting the test:', error);
-//       res.status(500).json({ message: 'Error starting the test' });
-//     }
-//   };
-//   module.exports = startTest;
 import Progress from '../models/progress.js';
 import express from "express";
 import cookieParser from 'cookie-parser';
