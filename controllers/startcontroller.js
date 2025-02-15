e8dfe07b4793f48c53dba2941aeeb37773a14f8e
maitreya-16 <maitreyavaidyam@gmail.com>
2025-02-16 00:02:36 +0530

    done

--- controllers/startcontroller.js
+++ controllers/startcontroller.js
@@ -1,68 +1,54 @@
-import { ProgressModel, UserModel } from "../config/db.js";
-import { QuestionModel } from "../config/db.js";
-import { Sequelize } from 'sequelize';
 
-const start = async (req, res) => {
-  try {
-    if(req.user==null){
-      return  res.status(404).json({error:"User not found"});
-    }
-    const user = req.user;
-    let questions;
-    try {
-      questions = await QuestionModel.findAll({
-        attributes: ['id', 'correct'],
-        where: { isJunior: user.isJunior },
-        order: Sequelize.literal('RANDOM()')
-      });
-    } catch (error) {
-      console.error('Error fetching questions:', error);
-      return res.status(500).json({ message: 'Internal server error' });
-    }
+import Progress from '../models/progress.js';
+import express from "express";
+import cookieParser from 'cookie-parser';
 
-    // Array of Question Ids and Correct Options idx 
-    const questionIds = questions.map((question) => question.id);
-    const correctOptions = questions.map((question) => question.correct);
-    try {
-      // Progress Table Created :)
-      const currentProgress = await ProgressModel.create({
-        userid: user.userId,
-        Questionsid: questionIds,
-        Correctans: correctOptions,
-        isJunior: user.isJunior,
-        Marks: 0,
-        Counter: 0,
-        Selectedans: [],
-      });
+const app = express();
+app.use(cookieParser());
+   
+async function getUser(){
+    try{
+        const token = req.cookies.token;
+            
+        if(!token){
+         return res.status(400).json({message:"error"});
+        }
+    
+        const user= jwt.verify(token , 'your_secret_key');
+    
+        if(!user){
+          return res.status(400).json({message:"error"});
+        }
+        return user.id;
+    }catch(error){
+        console.log("Starting user error", error)
+    };
+    
+}
 
-      if (!currentProgress) {
-        return res.status(404).json({ message: "Progress not found!" });
-      }
-
-      const questionId = currentProgress.Questionsid[currentProgress.Counter]; // Questions ID
-
-      const questionData = await QuestionModel.findOne({
-        where: { id: questionId },
-      });
-
-      if (!questionData) {
-        return res.status(404).json({ message: "Question not found!" });
-      }
-      const { question, options } = questionData;
-
-      const timeleft = 1800//30min start time 
-      return res.status(200).json({ question, options , timeleft });
-
-
-    } catch (error) {
-      console.error('Error creating progress record:', error);
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-
-  } catch (error) {
-    console.error("Error in start function:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-export default start;
+app.get('/start', async (req,res)=>{
+    try{
+        const user_id = getUser();
+        const qidarray= await Progress.findOne({
+            attributes: ['Questionsid'],
+            where:{
+                userid:user_id,
+            }
+        })
+        const firstQ=qidarray[0];
+        const qaObject = await mcq.findOne({
+            attributes: ['questions','options'],
+            where:{
+                id: firstQ,
+            }
+        })
+        
+        return res.status(200).json(
+            qaObject
+        )
+    
+    
+    }catch(error){
+        console.log("Error sending first question",error);
+    }}
+)
