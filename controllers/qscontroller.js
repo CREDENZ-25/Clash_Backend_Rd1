b099f6f06af042e05a59242fab5b80f81a53cf75
maitreya-16 <maitreyavaidyam@gmail.com>
2025-02-19 20:05:19 +0530

    Testing

--- controllers/qscontroller.js
+++ controllers/qscontroller.js
@@ -196,7 +196,7 @@ const next = async (req, res) => {
     });
   } catch (error) {
     console.error("Error in next function:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
--- controllers/startcontroller.js
+++ controllers/startcontroller.js
@@ -131,7 +131,7 @@ const start = async (req, res) => {
 
   } catch (error) {
     console.error("Error in start function:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
